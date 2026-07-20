aee959ef5c54fd95523fa530ce7f765c03b11c0d
Hassan Habib <hassanhabib@live.com>
2026-07-19 18:43:09 -0700

    ShouldRecallMemoriesFromFileAsync -> FAIL

--- Standard.Agents.Tests.Unit/Services/Foundations/Memorys/MemoryServiceTests.Logic.RecallMemoriesFromFile.cs
+++ Standard.Agents.Tests.Unit/Services/Foundations/Memorys/MemoryServiceTests.Logic.RecallMemoriesFromFile.cs
@@ -41,4 +41,45 @@ public partial class MemoryServiceTests
         this.fileBrokerMock.VerifyNoOtherCalls();
         this.loggingBrokerMock.VerifyNoOtherCalls();
     }
+
+    [Fact]
+    public async Task ShouldRecallMemoriesFromFileAsync()
+    {
+        // given
+        string memoryPath = CreateRandomString();
+        List<string> randomMemories = CreateRandomMemories();
+        IReadOnlyList<string> storedMemories = randomMemories;
+        IReadOnlyList<string> expectedMemories = storedMemories;
+
+        var fileMemoryService = new MemoryService(
+            fileBroker: this.fileBrokerMock.Object,
+            memoryPath: memoryPath,
+            loggingBroker: this.loggingBrokerMock.Object);
+
+        this.fileBrokerMock.Setup(broker =>
+            broker.FileExists(memoryPath))
+                .Returns(true);
+
+        this.fileBrokerMock.Setup(broker =>
+            broker.ReadAllLinesAsync(memoryPath))
+                .ReturnsAsync(storedMemories);
+
+        // when
+        IReadOnlyList<string> actualMemories =
+            await fileMemoryService.RecallMemoriesAsync();
+
+        // then
+        actualMemories.Should().BeEquivalentTo(expectedMemories);
+
+        this.fileBrokerMock.Verify(broker =>
+            broker.FileExists(memoryPath),
+                Times.Once);
+
+        this.fileBrokerMock.Verify(broker =>
+            broker.ReadAllLinesAsync(memoryPath),
+                Times.Once);
+
+        this.fileBrokerMock.VerifyNoOtherCalls();
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+    }
 }
