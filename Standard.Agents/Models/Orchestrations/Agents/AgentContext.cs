d78c5a549698fe446447d6e809c07b898fa55eaa
Hassan Habib <hassanhabib@live.com>
2026-07-28 05:34:49 -0700

    DATA: Add Remember To AgentContext Model

--- Standard.Agents/Models/Orchestrations/Agents/AgentContext.cs
+++ Standard.Agents/Models/Orchestrations/Agents/AgentContext.cs
@@ -18,5 +18,6 @@ public sealed record AgentContext
     public string RawReply { get; init; } = "";
 
     public string Result { get; init; } = "";
+    public string Remember { get; init; } = "";
     public AgentStatus Status { get; init; }
 }
