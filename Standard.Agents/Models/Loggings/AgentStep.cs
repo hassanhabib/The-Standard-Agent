89f0bd93ed978aa8144852e2fbe25633390a4cb0
Hassan Habib <hassanhabib@live.com>
2026-08-01 16:07:57 -0700

    DATA: Add AgentStep Enum

---
+++ Standard.Agents/Models/Loggings/AgentStep.cs
@@ -0,0 +1,13 @@
+// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the The Standard Software License (TSSL)
+// ---------------------------------------------------------------
+
+namespace Standard.Agents.Models.Loggings;
+
+public enum AgentStep
+{
+    Data = 0,
+    Decision = 1,
+    Direction = 2
+}
